78c550c3cd1dee66760ecc0527be00d3db392f81
RidgyTheFox ♥ <ridgy.dev@gmail.com>
2022-10-17 04:37:36 +0700

    Slightly refactored LocalizationPackage.cs
    Signed-off-by: RidgyTheFox ♥ <ridgy.dev@gmail.com>

--- Assets/Scripts/Localizations/LocalizationPackage.cs
+++ Assets/Scripts/Localizations/LocalizationPackage.cs
@@ -5,10 +5,11 @@ namespace Localizations
     public class LocalizationPackage
     {
         private string _packageName = "Default";
-        public string PackageName { get { return _packageName; } }
         private string _packageLanguageCode = "en_US";
-        public string PackageLanguageCode { get { return _packageLanguageCode; } }
         private string _packageAuthor = "Default";
+
+        public string PackageName { get { return _packageName; } }
+        public string PackageLanguageCode { get { return _packageLanguageCode; } }
         public string PackageAuthor { get { return _packageAuthor; } }
 
         public Dictionary<string, string> Strings = new Dictionary<string, string>();
