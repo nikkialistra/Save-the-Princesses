c545f27fc9719b48edca2b64ec91982b5306465e
RidgyTheFox ♥ <ridgy.dev@gmail.com>
2022-10-17 13:57:41 +0700

    Added realization into LocalizationProvider.cs
    Signed-off-by: RidgyTheFox ♥ <ridgy.dev@gmail.com>

--- Assets/Scripts/Localizations/LocalizationProvider.cs
+++ Assets/Scripts/Localizations/LocalizationProvider.cs
@@ -5,19 +5,52 @@ namespace Localizations
 {
     public static class LocalizationProvider
     {
-        private static string _PackageName { get; } = "Default";
-        private static string _PackageCode { get; } = "en_US";
-        private static string _PackageAuthor { get; } = "Default";
+        private static string _PackageName = "Default";
+        private static string _PackageCode = "en_US";
+        private static string _PackageAuthor = "Default";
+
         private static LocalizationPackage _loadedPackage = null;
+
+        public static string PackageName { get { return _PackageName; } }
+        public static string PackageCode { get { return _PackageCode; } }
+        public static string PackageAuthor { get { return _PackageAuthor; } }
 
         public static string GetLocalizedString(string stringId)
         {
-            throw new System.NotImplementedException($"{System.Reflection.MethodBase.GetCurrentMethod().Name} not implemented!");
+            if (_loadedPackage == null)
+            {
+                throw new LanguagePackageNotLoaded();
+                return null;
+            }
+
+            string value = "";
+            if (_loadedPackage.Strings.TryGetValue(stringId, out value))
+                return value;
+            else
+            {
+                throw new LocalizedStringNotFound();
+                return null;
+            }
         }
 
         public static bool LoadNewLanguagePackage(string languageCode)
         {
-            throw new System.NotImplementedException($"{System.Reflection.MethodBase.GetCurrentMethod().Name} not implemented!");
+            try
+            {
+                _loadedPackage = LocalizationLoader.GetPackageByCode(languageCode);
+                _PackageName = _loadedPackage.PackageName;
+                _PackageCode = _loadedPackage.PackageLanguageCode;
+                _PackageAuthor = _loadedPackage.PackageAuthor;
+
+                Debug.Log($"LOCALIZATION_PROVIDER: Language package with language code {languageCode} was loaded into provider.");
+                return true;
+            }
+            catch (LanguagePackageNotFoundException ex)
+            {
+                return false;
+            }
+
+            return false;
         }
     }
 }
