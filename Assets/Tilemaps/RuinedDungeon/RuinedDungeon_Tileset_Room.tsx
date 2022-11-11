24f465b167afb1a4f5279b33cfa0b05cd13de50d
nikkialistra <nikkialistra@gmail.com>
2022-11-11 10:56:48 +0300

    refactor: Move directories.

---
+++ Assets/Tilemaps/RuinedDungeon/RuinedDungeon_Tileset_Room.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="RoomPremier" tilewidth="32" tileheight="32" tilecount="110" columns="10">
+ <image source="../Sprites/RuinedDungeon/RuinedDungeon_Room.png" width="320" height="352"/>
+</tileset>
